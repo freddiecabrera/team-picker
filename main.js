8ab10ea7aa68198ca818f5fab7a0efbe4804f64f
freddiecabrera <freddiecabrera18@yahoo.com>
2016-02-25 21:58:22 -0800

    append players to available players and add class of overall rating

--- main.js
+++ main.js
@@ -6,21 +6,23 @@ function init(){
 var overAllRating;
 var baller;
 var ballers = [];
+
+//give me a drop down menu from 60 - 100
 var numOfPlayers = function() {
   var $select = $(".dropdownMenu");
   var showcounter = 100;
     for (i = 60; i <= showcounter; i++) {
         $select.append($('<option></option>').val(i).html(i));
     }
-}
+};
 
+//create a player object with name and rating and push into an array
+//append player to my available players
 var addAPlayer = function(){
   baller = $('#nameInput').val();
   overAllRating = $('.dropdownMenu').val();
   document.getElementById('nameInput').value = '';
+  var ballerElement = $('<p></p>').text(baller).addClass(overAllRating);
   ballers.push({name: baller, rating: overAllRating});
-
-
-  console.log(baller);
-  console.log(overAllRating);
-}
+  $('.availPlayerBox').append(ballerElement);//.setAttribute("id", overAllRating);
+};
