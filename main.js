2598849278fb292e6c1cb223852b30d63832f3c7
freddiecabrera <freddiecabrera18@yahoo.com>
2016-02-25 21:05:55 -0800

    get user input after button is cliked and store value in a variable

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 $(document).ready(init);
 function init(){
   numOfPlayers();
+  $('.button').click(addAPlayer);
 }
-
+var overAllRating;
+var baller;
 var numOfPlayers = function() {
   var $select = $(".dropdownMenu");
   var showcounter = 100;
     for (i = 60; i <= showcounter; i++) {
         $select.append($('<option></option>').val(i).html(i));
     }
+}
+
+var addAPlayer = function(){
+  baller = $('#nameInput').val();
+  document.getElementById('nameInput').value = '';
+
+
+  console.log(baller);
 }
